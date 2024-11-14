ab7de5dc9c282ceab9ed992b831b06be0334d782
Aguila <joseaguila3012@gmail.com>
2024-11-14 12:17:51 +0100

    test
    esto es una prueba

---
+++ Ejercicio1.kt
@@ -0,0 +1,14 @@
+import kotlin.concurrent.thread
+
+//array de 5 elementos pedir por teclado los 5 numeros e imprimirlos
+fun main() {
+    var num = IntArray(5) { 0 }
+    for (i in num.indices) {
+        println("dime el ${i + 1}")
+        num[i] = readln().toInt()
+    }
+    for (x in num.indices) {
+            println("el numero para la posicion ${x + 1} es: ${num[x]}...")
+            Thread.sleep(1000)
+        }
+}
